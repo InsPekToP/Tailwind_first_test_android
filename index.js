57101eddcb74ebe399ea300423c4af1874ccd68d
inspektop <ispektop@gmail.com>
2026-07-03 17:54:31 +0200

    first react-cdn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("hello")
+
+// const app = document.getElementById('app');
+
+// const header = document.createElement('h1');
+
+// const text = 'Text from JS'
+// const headerContent = document.createTextNode(text)
+
+// header.appendChild(headerContent)
+
+// app.appendChild(header)
+
+const app document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+root.render(<h1>Text from ReactCDN</h1>);
